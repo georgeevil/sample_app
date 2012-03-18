456a6404b469bb8d3a535c0aa6f9a926aaf198c6
George Chigrichenko <georgeevil@gmail.com>
2012-03-17 21:15:17 -0700

    Fixed an error about_link to about_path in _footer.html.erb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,11 @@
 module ApplicationHelper
+  
+   def logo
+    link_to image_tag("logo.png", :alt => "Sample App", :class => "round") , root_path
+  end
+  
   # Return a title on a per-page basis.
+  
   def title
     base_title = "Ruby on Rails Tutorial Sample App"
     if @title.nil?
@@ -8,4 +14,6 @@ module ApplicationHelper
       "#{base_title} | #{@title}"
     end
   end
+  
+ 
 end
